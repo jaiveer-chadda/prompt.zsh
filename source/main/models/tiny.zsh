407573a294aa02795d66e8693557c1f2e9536078
JV <jaiveer.chadda@gmail.com>
2026-06-02 03:31:06 +0400

    Created & commented `tiny` mode

--- source/main/models/tiny.zsh
+++ source/main/models/tiny.zsh
@@ -1,3 +1,19 @@
 #!/usr/bin/env zsh
 
-function prompt::tiny() { export PS1='$ '; }
+function prompt::tiny() {
+  export PS1=''
+
+  # if `$? == 0/1`, don't do anything. otherwise, dispay the error code in red
+  PS1+='%(?::%1(?::%1F%?%f ))'
+
+  # if there was an error, colour the following char red
+  #  otherwise, if we're in `sudo` mode, colour the following char blue
+  #  if we're in normal mode, colour it indigo
+  PS1+='%(?:%(!:%4F:%105F):%1F)'
+
+  # if we're in `sudo` mode, display a `#`, otherwise, display `$`
+  PS1+='%(!:#:$)'
+
+  # reset the colouring and add a trailing space
+  PS1+='%f '
+}
