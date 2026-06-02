68a0c07965d481b1b3504346c62f7261fb4d5372
JV <jaiveer.chadda@gmail.com>
2026-06-02 04:11:57 +0400

    Implemented the bash version of the prompt
    It's not technically a "bash" version, but it's just the version I use in my bash config

---
+++ source/main/models/bash.zsh
@@ -0,0 +1,46 @@
+#!/usr/bin/env zsh
+
+function prompt::bash() {
+  local -r  formatted_path="${PWD/#$HOME/~}"
+  local -ra path_array=( "${(s:/:)formatted_path}" )
+  local -ra colours=( 1 3 2 6 4 5 )
+
+  local -i 10 i colour_num
+  local colour_esc output
+
+  export PS1=
+
+  for i in {1..$#path_array}; {
+    colour_num=$(( colours[ ( ( i - 1 ) % $#colours ) + 1 ] + 8 ))
+    PS1+="%${colour_num}F$path_array[i]"
+    if (( i != $#path_array )) PS1+=$'%{\e[2m%}/%{\e[22m%}'
+  }
+
+  PS1+=' %105F$%f '
+}
+
+# function _set_ps1 () {
+#   local -ra colours=( 1 3 2 6 4 5 )
+#   local -r formatted_path="${PWD/#$HOME/\~}"
+#   local -a path_array=()
+#
+#   IFS='/' read -ra path_array <<< "$formatted_path"
+#
+#   local -i i colour_num
+#   local colour_esc path_segment output
+#
+#   for i in "${!path_array[@]}"; do
+#     colour_num="${colours[ i % ${#colours[@]} ]}"
+#     colour_esc=$'\[\e[9'"${colour_num}m\]"
+#
+#     path_segment="${path_array[i]}"
+#
+#     output+="$colour_esc$path_segment"
+#     # print a slash after every segment except the last
+#     (( i != ${#path_array[@]} - 1 )) && output+=$'\[\e[2m\]/\[\e[22m\]'
+#     output+=$'\[\e[m\]'
+#   done
+#
+#   output+=$'\[\e[38;5;105m\] $\[\e[m\] '
+#   export PS1="$output"
+# }
