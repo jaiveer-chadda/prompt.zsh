707868f025c86b8da3faffd669f79a8f63442812
JV <jaiveer.chadda@gmail.com>
2026-06-29 18:28:18 +0530

    Added a bunch of new icons to the path function, so now pretty much every common directory I have, has its own icon

--- source/main/proc/get-path.zsh
+++ source/main/proc/get-path.zsh
@@ -2,14 +2,57 @@
 
 function prompt::get_path () {
   local -rA dir_icons=(
-    'Desktop'        ''  # \uf4a9
-    'Documents'      ''  # \uf016
-    'Downloads'      ''  # \uf409
-    'CS'             ' ' # \uf121
-    'y_settings_etc' ''  # \uf013
-    '.config'       '.'  # \uf013
-    'Shellscript'    ''  # \ue691
-    'zsh'            ''  # \uf489
+    # General                 # Desktop
+    'bash'          ''       'cs'                  ' '
+    'man'           '󰺄'       'labour'              '󰧲'
+    'shared'        '󱒃'       'screenshots'         '󰄀'
+    'temp'          '󰔛'       'university work'     ' '
+    'vscode'        ''
+    'zsh'           ''
+
+    # General Dev             # CS
+    '.git'          '󰊢'       'applescript'         ''
+    '.idea'         ''       'c'                   ''
+    '.vscode'       ''       'c#'                  ''
+    'archive'       ''       'discord'             ' '
+    'archives'      ''       'java'                ''
+    'backup'        '󰁯'       'karabiner'           ''
+    'backups'       '󰁯'       'man pages'           '󰺄'
+    'build'         ''       'python'              ''
+    'export'        ''       'raycast'             ''
+    'out'           ''       'rust'                ''
+    'output'        ''       'shellscript'         ''
+    'resources'     '󱂸'       'swift'               ''
+    'source'        '󰴉'       'vs_code'             ''
+    'test'          '󰙨'       'web'                 '󰖟'
+    'tests'         '󰙨'       'x_Automation'        '󰚩'
+    '#'             ''        'y_settings_etc'      ''
+    '#'             ''        'z_other'             '󱝏'
+
+    # Root (/)                # Documents
+    'applications'  ''       'apps'                ''
+    'bin'           ''       'audio'               ''
+    'dev'           '󰾰'       'backups:archives'    '󱝏'
+    'fd'            '󰮗'       'images'              ''
+    'private'       ' '      'videos'              '󰨜'
+    'sbin'         's'
+    'system'        '󰡀 '      # Specific Dirs
+    'tmp'           '󰔛'       'com~apple~clouddocs' ' '
+    'users'         ' '      'homebrew'            '󱄖'
+    'volumes'       ''
+
+    # Home (~)
+    '.config'      '.'
+    '.local'        ''
+    '.trash'        ''
+    'desktop'       ''
+    'documents'     ''
+    'downloads'     ''
+    'library'       ''
+    'movies'        '󰿎'
+    'music'         ''
+    'pictures'      ''
+    'public'        '󰛍'
   )
 
   path_arr=( "${(@s:/:)PWD/#$HOME/~}" )
@@ -23,7 +66,7 @@ function prompt::get_path () {
 
   for i in {1..$path_len}; {
     dir_name="$path_arr[i]"
-    icon="$dir_icons[$dir_name]"
+    icon="$dir_icons[$dir_name:l]"
 
     if (( do_short && i != path_len )) {
       path_arr[i]="${icon:-$dir_name}"
